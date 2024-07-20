3b8a5a4810bd5d92f64875212a65db4d7d36a469
Harshal Pawshekar <harshal.pawshekar@greytip.com>
2024-07-20 15:16:38 +0530

    Initial commit

--- apps/host-app/src/app/app.config.ts
+++ apps/host-app/src/app/app.config.ts
@@ -1,10 +1,19 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, isDevMode, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { reducers } from './reducers';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
-  ],
+    provideHttpClient(),
+    provideStore(reducers),
+    provideEffects(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+  ]
 };
